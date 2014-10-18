ea52db1736842856b4f4582c5a1263386bccb76f
Jeffcwng <ng.jeffrey@gmail.com>
2014-10-18 10:24:37 -0700

    test for view and form

--- xkcd/tests.py
+++ xkcd/tests.py
@@ -11,6 +11,27 @@ from xkcd.models import Person, Like
 # from xkcd.test_utils import run_pyflakes_for_package, run_pep8_for_package
 
 
+class FormTestCase(TestCase):
+    def test_clean_username_exception(self):
+        # Create a player so that this username we're testing is already taken
+        Person.objects.create_user(username='test-user')
+
+        # set up the form for testing
+        form = EmailUserCreationForm()
+        form.cleaned_data = {'username': 'test-user'}
+
+        # use a context manager to watch for the validation error being raised
+        with self.assertRaises(ValidationError):
+            form.clean_username()
+
+    def test_clean_username_inverse(self):
+
+        Person.objects.create_user(username='test-user')
+        form = EmailUserCreationForm()
+        form.cleaned_data = {'username': 'tom'}
+        self.assertEqual(form.clean_username(), "tom")
+
+
 class ViewTestCase(TestCase):
     def test_home_page(self):
         print "test home page"
