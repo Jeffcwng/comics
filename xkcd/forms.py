9f3b4179477d8121d38848613f0f01396a78f3da
Mike Lee <mike.seung.lee@gmail.com>
2014-10-18 01:41:47 -0700

    Barebones project now follows pep8 and some basic unit tests for models

--- xkcd/forms.py
+++ xkcd/forms.py
@@ -1,8 +1,6 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.forms import ModelForm
-from django.contrib.auth.models import *
-from models import *
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# from django.forms import ModelForm
 
 # class EmailUserCreationForm(UserCreationForm):
 #     email = forms.EmailField(required=True)
