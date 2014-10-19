f3213df691d3cfbbf2dc6ebd7e6778b5804d5af8
Miguel A Barbosa <miguelabarbosa@gmail.com>
2014-10-19 11:36:01 -0700

    group meeting commit

--- xkcd/data/fetchcomics.py
+++ xkcd/data/fetchcomics.py
@@ -5,7 +5,7 @@ import glob
 
 
 def get_all_comics_part_1():  # grab all comics from 1 - 404 (because 404 is missing)
-    with open('xkcd/data/part1.txt', 'w') as f:
+    with open('xkcd/data/part1.json', 'w') as f:
         for n in range(1, 404):
             each_comic = requests.get("http://xkcd.com/{}/info.0.json".format(n)).json()
             json.dump(each_comic, f)
@@ -13,14 +13,14 @@ def get_all_comics_part_1():  # grab all comics from 1 - 404 (because 404 is mis
 
 def get_all_comics_part_2():  # grab all comics from 405 - current date
     latest_comic = requests.get("http://xkcd.com/info.0.json").json()
-    with open('xkcd/data/part2.txt', 'w') as f:
+    with open('xkcd/data/part2.json', 'w') as f:
         for n in range(405, latest_comic['num'] + 1):
             each_comic = requests.get("http://xkcd.com/{}/info.0.json".format(n)).json()
             json.dump(each_comic, f)
 
 
 def add_both_files():  # combine both files
-    read_files = glob.glob("xkcd/data/*.txt")
+    read_files = glob.glob("xkcd/data/*.json")
     with open("xkcd_allcomics.json", "wb") as outfile:
         for f in read_files:
             with open(f, "rb") as infile:
