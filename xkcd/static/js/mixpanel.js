1a5dc9f6fa3beb6a40b7401f9712330c32a4931d
Miguel A Barbosa <miguelabarbosa@gmail.com>
2014-10-23 15:10:21 -0700

    adding mixpanel details

--- xkcd/static/js/mixpanel.js
+++ xkcd/static/js/mixpanel.js
@@ -2,6 +2,23 @@
  * Created by miguelbarbosa on 10/23/14.
  */
 
+
+$(document).ready(function() {
+    $("#register").click(function () {
+      mixpanel.identify("13487");
+      mixpanel.people.set({
+            "$username": "Joe",
+            "$first_name": "Joe",
+            "$last_name": "Joe",
+            "$email": "joe.doe@example.com",
+            "$created": "2013-04-01T09:02:00"
+        });
+
+    });
+});
+
+
+
 $(document).ready(function() {
     $("#like_this").click(function () {
         mixpanel.track("Like button clicked");
@@ -13,3 +30,4 @@ $(document).ready(function() {
         mixpanel.track("Logged In");
     });
 });
+
